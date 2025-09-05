4eca0f03bd26f59a0a9f3391c006eae3652f3d8d
keokukzh <keokukmusic@gmail.com>
2025-09-05 08:46:10 +0200

    test: reset phpunit config & bootstrap (UTF-8 no BOM)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,8 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../aidev-plugin-starter/aidev-plugin-starter.php';
+declare(strict_types=1);
+
+// Composer autoload
+require_once __DIR__ . "/../vendor/autoload.php";
+
+// Plugin laden
+require_once __DIR__ . "/../aidev-plugin-starter/aidev-plugin-starter.php";
